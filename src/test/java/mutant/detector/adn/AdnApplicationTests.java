79516fede83c05356f57c61b0f88a02c8ec81cc9
Lihuen Figueroa <lihuen.figueroa@landscape.cl>
2024-10-31 18:55:12 -0300

    initial commit

---
+++ src/test/java/mutant/detector/adn/AdnApplicationTests.java
@@ -0,0 +1,13 @@
+package mutant.detector.adn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
